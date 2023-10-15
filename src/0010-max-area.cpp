cad5db0c048b3431ab4df1bbfce89bf321cbf0e8
Alexander Städing <alexanderstaeding@gmail.com>
2023-10-15 16:14:00 +0200

    0010: Clean up and add more test cases

--- src/0010-max-area.cpp
+++ src/0010-max-area.cpp
@@ -1,11 +1,14 @@
 #include "0010-max-area.h"
 
 int maxArea(const std::vector<int> &height) {
+  if (height.empty()) {
+    return 0;
+  }
   int low = 0;
   int high = height.size() - 1;
   int result = 0;
   while (low != high) {
-    int area = std::min((int) height[low], (int) height[high]) * (high - low);
+    int area = std::min(height[low], height[high]) * (high - low);
     if (area > result) {
       result = area;
     }
--- test/0010-max-area-test.cpp
+++ test/0010-max-area-test.cpp
@@ -8,3 +8,31 @@ TEST_CASE("Example 1", "[maxArea]") {
 TEST_CASE("Example 2", "[maxArea]") {
   REQUIRE(maxArea({1, 1}) == 1);
 }
+
+TEST_CASE("Descending heights", "[maxArea]") {
+  REQUIRE(maxArea({7, 6, 5, 4, 3, 2, 1}) == 12);
+}
+
+TEST_CASE("Ascending heights", "[maxArea]") {
+  REQUIRE(maxArea({1, 2, 3, 4, 5, 6, 7}) == 12);
+}
+
+TEST_CASE("Constant height", "[maxArea]") {
+  REQUIRE(maxArea({5, 5, 5, 5, 5}) == 20);
+}
+
+TEST_CASE("Single element", "[maxArea]") {
+  REQUIRE(maxArea({2}) == 0);
+}
+
+TEST_CASE("Two elements with different heights", "[maxArea]") {
+  REQUIRE(maxArea({2, 1}) == 1);
+}
+
+TEST_CASE("Empty container", "[maxArea]") {
+  REQUIRE(maxArea({}) == 0);
+}
+
+TEST_CASE("All zero heights", "[maxArea]") {
+  REQUIRE(maxArea({0, 0, 0, 0}) == 0);
+}
